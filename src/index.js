d91d3e230380559382f9ce1cabfb692b38c566e5
Your Name <you@example.com>
2025-02-25 10:04:52 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+
+dotenv.config({
+  path : './env'
+})
+
+connectDB()
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// iffi function of javascript to connect to the database immidiately 
+// First approach to connect Database 
+/*
+
 import express from 'express';
 const app = express();
+( async () =>{
+  try{
+   await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on('error', (error) => {
+      console.log('Error connecion to database');
+      throw error;
+    })
 
-app.get('/',(req,res) => {
-res.send('server ready to use');
-})
-
-app.get('/api/Product', (req,res) => {
-  const Product = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 100,
-      image: 'https://via.placeholder.com/150'
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 200,
-      image: 'https://via.placeholder.com/150'
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      price: 300,
-      image: 'https://via.placeholder.com/150'
-    },
-  ]
-  res.send(Product);
-})
-
-  const port = process.env.PORT || 5000;
-
-  app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    })
+  } catch(error){
+    console.log("ERROR: ", error);
   }
-)
+})();
+*///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
